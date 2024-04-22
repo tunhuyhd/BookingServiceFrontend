847976aeba45cd9ac670e8cfd7fcdbac0f429800
tunghuy22t2 <nguyentunghuy02@gmail.com>
2024-04-22 15:34:11 +0700

    setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'company', loadChildren: () => import('./company/company.module').then(m => m.CompanyModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
